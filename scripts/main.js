f82a83c0d663a2556d4cc9150c735f789bea51a7
Noah Ojile <104537320+Noahojile@users.noreply.github.com>
2022-11-27 01:03:48 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','C:\Users\Hp\Documents\web_projects\images\profile_image.PNG');
+  } else {
+    myImage.setAttribute ('src','C:\Users\Hp\Documents\web_projects\images\profile_image.PNG');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'welcome to my portfolio, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'welcome back, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
